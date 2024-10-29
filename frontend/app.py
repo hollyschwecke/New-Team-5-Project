fd8ed50ba95a2b10db9c90577d555909bd9c9ae0
clueking2 <clairelueking@gmail.com>
2024-10-29 15:16:51 -0600

    baby stages of main product page, need to connect databse for rest of changes

---
+++ frontend/app.py
@@ -0,0 +1,12 @@
+#potentially needing to use this file to help connect with database and flask and backend
+
+
+# from flask import Flask, jsonify
+
+# app = Flask(__name__)
+
+# @app.route('/get_username')
+# def get_username():
+#     #fetched the username from your database
+#     username = "sajfhoafh"  # Replace actual fetched username
+#     return jsonify(username=username)
