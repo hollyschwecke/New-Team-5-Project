a8066e9a6e87222df34b5d198efba663446ab369
Claire Lueking <120064200+clueking2@users.noreply.github.com>
2024-11-05 13:53:50 -0700

    cleaned up routes again on app.py

--- app.py
+++ app.py
@@ -1,39 +1,22 @@
-#potentially needing to use this file to help connect with database and flask and backend
+#routes for render and flask
 
 import psycopg2
 from flask import Flask, render_template
 app = Flask(__name__)
-import sqlite3
+# import sqlite3
 
 @app.route('/')
 def logging_in():
-    return render_template('./static/login.html') #username=username
+    return render_template('login.html') 
 
 @app.route('/search')
 def searching():
-    return render_template('./static/searchpage.html') #username=username
+    return render_template('searchpage.html') 
 
 @app.route('/mainproductlist')
 def main_product():
-    return render_template('./static/mainproductlist.html') #username=username
+    return render_template('mainproductlist.html') 
 
 
-# def get_username():
-#     # Connect to your database
-#     conn = sqlite3.connect('your_database.db')
-#     cursor = conn.cursor()
-    
-#     # Fetch the username (modify the query according to your database structure)
-#     cursor.execute("SELECT username FROM users WHERE id = 1")  # Example query
-#     username = cursor.fetchone()
-    
-#     conn.close()
-    
-#     return username[0] if username else "Guest"
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-    # logging_in()
-    # searching()
-    # main_product()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
