10ece290eb32be94fcbf3b02e6ee870867eac389
hollyschwecke <143843851+hollyschwecke@users.noreply.github.com>
2024-11-14 15:09:42 -0700

    added addproduct function

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-#routes for render and flask
+wol#routes for render and flask
 
 import psycopg2
 # import petstore.db
 # import login.db
 # import suppliers.db
 from flask import Flask, render_template, request, redirect, url_for
-app = Flask(__name__)
+from datetime import datetime
 import sqlite3
+
+app = Flask(__name__)
+
 
 def check_user_credentials(username, password):
     conn = sqlite3.connect('petstore.db')
@@ -21,7 +24,7 @@ def check_user_credentials(username, password):
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('index.html')
 
 # handles login authentication
 @app.route('/login', methods=['POST'])
@@ -49,7 +52,48 @@ def main_product():
 
 @app.route('/addproduct')
 def adding_product():
-    return render_template('addproduct.html') #, tables=[petstore.to_html(classes='data', header="true")])
+    # get form data
+    name = request.form['product-name']
+    category = request.form['category']
+    price = float(request.form['price'])
+    description = request.form['description']
+    available_quantity = int(request.form['quantity'])
+    date_added = datetime.not().strftime('%Y-%m-%d %H:%M:$S')
+
+    # handle image uploads
+    image_files = request.files.getlist('picture')
+    image_paths = []
+    for img in image_files:
+        if img:
+            # save each image to a specific folder and store the file path
+            img_path = f"uploads/{img.filename}"
+            img.save(img_path)
+            image_paths.append(img_path)
+
+    # create connection to db
+    conn.sqlite3('petstore.db')
+    cursor = conn.cursor()
+
+    # insert product data into products table
+    cursor.execute('''
+        INSERT INTO Products (name, description, price, category, available_quantity, date_added)
+        VALUES (?, ?, ?, ?, ?, ?)
+        ''', (name, description, price, category, available_quantity, date_added))
+
+    # get id of last inserted product
+    product_id = cursor.lastrowid
+
+    # insert image paths into the ProductImages table
+    for img_path in image_paths:
+        cursor.execute('''
+            INSERT INTO ProductImages (product_id, image_path)
+            VALUES (?, ?)
+            ''', (product_id, image_path))
+
+    conn.commit()
+    conn.close()
+    
+    return render_template('addproduct.html', success=True) #, tables=[petstore.to_html(classes='data', header="true")])
 
 @app.route('/createaccount')
 def creating_account():
