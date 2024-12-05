9d2bd44ea66e8d03af14bfd5f7e851ce370acbf7
clueking2 <clairelueking@gmail.com>
2024-12-05 09:14:44 -0700

    app.py

--- app.py
+++ app.py
@@ -277,7 +277,7 @@ def search():
     cur = None
     username = None
     try:
-        conn = psycopg2.connect("postgresql://schwecke_lab10_database_user:4NeoO85Ipw8AavH2X3IOOflP6aOlVbfA@dpg-csluug1u0jms73b9eflg-a/schwecke_lab10_database")
+        conn = psycopg2.connect("postgresql://schwecke_lab10_database_user:4NeoO85Ipw8AavH2X3IOOflP6aOlVbfA@dpg-csluug1u0jms73b9eflg-a.oregon-postgres.render.com/schwecke_lab10_database")
         cur = conn.cursor()
 
         # Fetch the username from the database (if user is logged in)
